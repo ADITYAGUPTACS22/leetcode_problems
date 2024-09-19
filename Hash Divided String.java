0d65f6f06ea0bb1971d7dacd33f16fa7a95b247f
Aditya gupta <122453566+ADITYAGUPTACS22@users.noreply.github.com>
2024-09-19 17:40:13 +0530

    leet code 3271

---
+++ Hash Divided String.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String stringHash(String s, int k) {
+        int n = s.length();
+        int numSubstrings = n / k;
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < numSubstrings; i++) {
+            String substring = s.substring(i * k, (i + 1) * k);
+            
+            int sum = 0;
+            for (char c : substring.toCharArray()) {
+                sum += c - 'a';
+            }
+            
+            int hashedChar = sum % 26;
+            
+            char resultChar = (char) ('a' + hashedChar);
+            
+            result.append(resultChar);
+        }
+        
+        return result.toString();
+    }
+}
