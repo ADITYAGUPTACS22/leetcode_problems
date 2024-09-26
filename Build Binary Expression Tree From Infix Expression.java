bbc4b44866c93528160fa1b1c0ac6a4ba4fb2e3a
Aditya gupta <122453566+ADITYAGUPTACS22@users.noreply.github.com>
2024-09-26 11:06:29 +0530

    1597

---
+++ Build Binary Expression Tree From Infix Expression.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+/**
+ * Friend_Requests_I
+ */
+class Node{
+
+    char val;
+    Node left , right;
+    public Node(char val){
+        this.val=val;
+    }
+}
+public class Build_Binary_Expression_Tree_From_Infix_Expression {
+
+        public String getPostfix(String s){
+            StringBuilder sb=new StringBuilder();
+            Stack<Character> st=new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char ch=s.charAt(i);
+                if(ch>'0' && ch<='9'){
+                    sb.append(ch);
+                }
+                else if(ch=='('){
+                    st.push(ch);
+                }
+                else if(ch==')'){
+                    while(st.peek() != '('){
+                        sb.append(st.pop());
+                    }
+                    st.pop();
+                }
+                else{
+                    while(!st.isEmpty() && getValue(st.peek()) >= getValue(ch)){
+                        sb.append(st.pop());
+                    }
+                    st.push(ch);
+                }
+            }
+            while(!st.isEmpty()){
+                sb.append(st.pop());
+            }
+            return sb.toString();
+        }
+
+        public int getValue(char ch){
+            if(ch=='*' || ch=='/'){
+                return 2;
+            }
+            else if(ch=='+' || ch=='-'){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }
+    }
